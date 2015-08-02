f664c38ed3dbc404488659fc8d5b4da8edef9a06
Jonathan Tsang <chewymeister88@googlemail.com>
2015-08-02 17:08:28 +0100

    Include for testing submodule update feature

--- submodule_temp.rb
+++ submodule_temp.rb
@@ -2,4 +2,8 @@ class SubmoduleTemp
   def this_will_test_submodule_headless_state_conflict
     "This will test to see if I can fix headless state conflicts with submodules!!"
   end
+
+  def testing_submodule_update
+    "This will test to see if I can update submodules correctly"
+  end
 end
